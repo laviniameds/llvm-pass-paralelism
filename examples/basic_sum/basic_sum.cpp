ad313801b4030dd507f5996b98e53886272df85a
Lavinia Miranda <laviniameds@icloud.com>
2020-08-19 18:53:12 -0300

    create test script to build and run the model pass

--- examples/basic_sum/basic_sum.cpp
+++
@@ -1,4 +0,0 @@
-int sum(int a, int b){
-    int c = a + b;
-    return c;
-}
---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# build project
+cd build/ && cmake .. && make && cd .. && echo ""\
+
+# testing pass
+src=$1
+src2=$2
+obj=${1%.c}.ll
+opt=${1%.c}.opt.ll
+
+clang-10 -S -emit-llvm ${src} -g3 -O0 -Xclang -disable-O0-optnone -o ${obj}
+
+opt-10 -S -mem2reg ${obj} > ${opt}
+
+opt-10 -load build/parallelism/libParallelismPass.so -parallelism < ${opt} > /dev/null
