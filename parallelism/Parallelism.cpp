4bbc71f7d548c4f8b69a3733f17ba2f13e3d320a
Lavinia Miranda <laviniameds@icloud.com>
2020-08-19 18:51:15 -0300

    bug fix model pass

--- parallelism/Parallelism.cpp
+++ parallelism/Parallelism.cpp
@@ -1,28 +1,20 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/IR/LegacyPassManager.h"
-#include "llvm/Transforms/IPO/PassManagerBuilder.h"
 using namespace llvm;
 
-namespace {
-  struct ParallelismPass : public FunctionPass {
+namespace{
+  struct ParallelismPass : public FunctionPass{
     static char ID;
     ParallelismPass() : FunctionPass(ID) {}
 
-    virtual bool runOnFunction(Function &F) {
+    virtual bool runOnFunction(Function &F){
       errs() << "I saw a function called " << F.getName() << "!\n";
       return false;
     }
   };
-}
+} 
 
 char ParallelismPass::ID = 0;
 
-static void registerParallelismPass(const PassManagerBuilder &,
-                         legacy::PassManagerBase &PM) {
-  PM.add(new ParallelismPass());
-}
-static RegisterStandardPasses
-  RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
-                 registerParallelismPass);
+static RegisterPass<ParallelismPass> X("parallelism", "Parallelism Pass");
