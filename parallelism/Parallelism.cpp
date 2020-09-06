8adb88ceb02d77ab5b6fd05afcd80d63b737bb13
Lavinia Miranda <laviniameds@icloud.com>
2020-09-06 17:10:05 -0300

    run through basic blocks and identify instruction types

--- parallelism/Parallelism.cpp
+++ parallelism/Parallelism.cpp
@@ -1,6 +1,9 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/IntrinsicInst.h"
+#include "llvm/IR/DebugInfoMetadata.h"
 using namespace llvm;
 
 namespace{
@@ -8,10 +11,48 @@ namespace{
     static char ID;
     ParallelismPass() : FunctionPass(ID) {}
 
-    virtual bool runOnFunction(Function &F){
-      errs() << "I saw a function called " << F.getName() << "!\n";
+    virtual bool runOnFunction(Function &llvm_function){
+      errs() << "I saw a function called " << llvm_function.getName() << "!\n";
+      runOnBasicBlocks(llvm_function);
       return false;
     }
+
+    void runOnBasicBlocks(Function &llvm_function){   
+      for (auto &bb_llvm: llvm_function.getBasicBlockList())
+        runOnInstructions(llvm_function, bb_llvm);
+
+    }
+
+    int getOperatorType(int llvm_opCpde){
+      if(llvm_opCpde >= llvm::Instruction::BinaryOps::BinaryOpsBegin && llvm_opCpde < llvm::Instruction::BinaryOpsEnd){
+        return 1;
+      }
+      else if (llvm_opCpde >= llvm::Instruction::TermOpsBegin && llvm_opCpde < llvm::Instruction::TermOpsEnd){
+        return 2;
+      }
+      else if(llvm_opCpde >= llvm::Instruction::OtherOpsBegin && llvm_opCpde < llvm::Instruction::OtherOpsEnd){
+        return 3;
+      }
+      else
+        return -1;
+    }
+    
+    void runOnInstructions(Function &llvm_function,	BasicBlock &bb_llvm){
+      for (auto &llvm_instruction : bb_llvm.getInstList()) {
+        int llvm_opCpde = llvm_instruction.getOpcode();
+        int opType = getOperatorType(llvm_opCpde);
+
+        switch (opType) {
+          case 1:
+            errs() << "Binary Instruction" << "\n";
+            break;
+          default:
+            errs() << "Other Instruction" << "\n";
+            break;
+        }
+      }
+  }
+
   };
 } 
 
