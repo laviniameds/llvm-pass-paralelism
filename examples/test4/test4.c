6936afddbb494dade9302c983e4cd81e81ef076d
Lavinia Miranda <laviniameds@icloud.com>
2020-09-16 21:01:03 -0300

    example test4 multiple basic blocks

---
+++ examples/test4/test4.c
@@ -0,0 +1,14 @@
+int f(int a, int b, int c){ 
+  int x1,x2,x3,x4,x5,x6,x7,x8;;
+  x1 = a*b;
+  x2 = a+b;
+  if (x1>x2){
+    x3 = x2+c;
+    x4 = x3+x1;
+    x5 = x4+c;
+    x6 = x5+x3;
+    x7 = x4+x2;
+  }
+  x8 = x1*x7;
+  return x8; 
+}
