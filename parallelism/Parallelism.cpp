5e50cad29b5f54dd120a85dd731bca16f1b228b9
Lavinia Miranda <laviniameds@icloud.com>
2020-09-17 11:38:05 -0300

    phi bug

--- parallelism/Parallelism.cpp
+++ parallelism/Parallelism.cpp
@@ -23,6 +23,7 @@ namespace {
 		std::map<Instruction*, int> map_instr_cycle_alap;
 		std::map<Instruction*, int>::iterator it_map_instr_cycle;
 		std::map<Instruction*, int>::reverse_iterator r_it_map_instr_cycle;
+		std::vector<Instruction*> inst_list;
 
 		//define vars to cycle
 		int greatest_cycle = 0;
@@ -31,39 +32,12 @@ namespace {
 		//number of basic blocks
 		int cont_bb = 0;
 
-		//run on each file function
-		virtual bool runOnFunction(Function &llvm_function) {
-			//print funcion name
-			errs() << "Function: " << llvm_function.getName() << "\n";
-			//run on each function basic block 
-			runOnBasicBlocks(llvm_function);
-			
-			return false;
-		}
-
-		//run on each funcion basic block
-		void runOnBasicBlocks(Function &llvm_function) {
-			//foreach basic block
-			for (auto &bb_llvm : llvm_function.getBasicBlockList()){
-
-				map_instr_cycle_asap.clear();
-				map_instr_cycle_alap.clear();
-				greatest_cycle = 0;
-				cycle = 0;
-
-				errs() << "\n\n-- Basic Block: " << ++cont_bb << " --\n";
-
-				//run on basick block instructions
-				runOnInstructions(llvm_function, bb_llvm);
-			}
-		}
-
 		//Cycles As Soon As Possible 
 		int asap(Instruction &llvm_instruction, BasicBlock &llvm_bb){
 			//if the instruction parent is not this same basic block, then it has no dependencies on this basic block. 
 			//Its parent comes from another basic block above. So its cycle is -1.
 			if (llvm_instruction.getParent() != &llvm_bb)
-				return -1;
+				return 0;
 
 			//define var cycle
 			int cycle = 0;
@@ -79,7 +53,7 @@ namespace {
 				//cast operand value as a instruction
 				Instruction *inst = llvm::dyn_cast<llvm::Instruction>(llvm_instruction.getOperand(i));
 				//if it is a instruction
-				if (inst){
+				if (inst && !inst->isTerminator()){
 					//cycle is always the max between the current value and the cycle value returned from recursion
 					cycle=std::max(cycle,1+asap(*inst,llvm_bb)); 
 				}
@@ -89,10 +63,6 @@ namespace {
 
 			//get greatest number of cycles
 			if(cycle > greatest_cycle) greatest_cycle = cycle;
-			
-			//if it is a terminator instruction, it belongs to the last cycle
-			if(llvm_instruction.isTerminator())
-				cycle = greatest_cycle;
 
 			//return the cycle value
 			return cycle;
@@ -116,7 +86,7 @@ namespace {
 				//cast "use value" as a instruction
 				Instruction *inst = llvm::dyn_cast<llvm::Instruction>(*it_user_value);
 				//if it is a instruction
-				if (inst){
+				if (inst && !inst->isTerminator()){
 					//cycle is always the min between the current value and the cycle value returned from recursion
 					cycle=std::min(cycle,alap(*inst,llvm_bb, greatest_cycle, cycle)-1); 
 				}
@@ -124,12 +94,18 @@ namespace {
 			//insert instruction and cycle value in the map
 			map_instr_cycle_alap.insert({&llvm_instruction, cycle});
 
-			//if it is a terminator instruction, it belongs to the last cycle
-			if(llvm_instruction.isTerminator())
-				cycle = greatest_cycle+1;
-
 			//return the cycle value
 			return cycle;
+		}
+
+		void filterInstructions(Function &llvm_function, BasicBlock &bb_llvm){
+			for (auto &llvm_instruction : bb_llvm.getInstList()) {
+				//cast instruction to debug instruction
+				auto debug_value_instruction = llvm::dyn_cast<llvm::DbgValueInst>(&llvm_instruction);
+				//if its not a debug instruction
+				if(!debug_value_instruction && !llvm_instruction.isTerminator())
+					inst_list.push_back(&llvm_instruction);
+			}
 		}
 		
 		//run on instructions
@@ -137,15 +113,10 @@ namespace {
 			//print ASAP Cycles
 			errs() << "\n--- ASAP ---\n";
 			//foreach instruction on basick block
-			for (auto &llvm_instruction : bb_llvm.getInstList()) {
-				//cast instruction to debug instruction
-				auto debug_value_instruction = llvm::dyn_cast<llvm::DbgValueInst>(&llvm_instruction);
-				//if its not a debug instruction
-				if(!debug_value_instruction){
-					cycle = asap(llvm_instruction,bb_llvm);
+			for (auto &llvm_instruction : inst_list) {
+					cycle = asap(*llvm_instruction,bb_llvm);
 					//print the instruction asap cycle
-     				errs() << " Cycle "<< cycle <<  ": "<< llvm_instruction.getOpcodeName() << " (" << llvm_instruction << ")\n";
-				}
+     				errs() << " Cycle "<< cycle <<  ": "<< llvm_instruction->getOpcodeName() << "( " << *llvm_instruction << " )\n";
 			}
 
 			//ALAP Cycles
@@ -156,8 +127,45 @@ namespace {
 			}
 			//print ALAP Cycles
 			for (it_map_instr_cycle = map_instr_cycle_alap.begin(); it_map_instr_cycle != map_instr_cycle_alap.end(); ++it_map_instr_cycle) {
-				errs() << " Cycle "<< it_map_instr_cycle->second <<  ": "<< it_map_instr_cycle->first->getOpcodeName() << " (" << *it_map_instr_cycle->first << ")\n";
+				errs() << " Cycle "<< it_map_instr_cycle->second <<  ": "<< it_map_instr_cycle->first->getOpcodeName() << "( " << *it_map_instr_cycle->first << " )\n";
 			}	
+
+			//print mobility
+			errs() <<"\n\n--- Mobility ---\n";
+			for(it_map_instr_cycle = map_instr_cycle_asap.begin(); it_map_instr_cycle != map_instr_cycle_asap.end(); ++it_map_instr_cycle){
+				int c_alap = map_instr_cycle_alap.find(it_map_instr_cycle->first)->second;
+				errs() << it_map_instr_cycle->first->getOpcodeName() << ": " << (c_alap - it_map_instr_cycle->second) << "\n";
+			}
+		}
+
+		//run on each funcion basic block
+		void runOnBasicBlocks(Function &llvm_function) {
+			//foreach basic block
+			for (auto &bb_llvm : llvm_function.getBasicBlockList()){
+
+				map_instr_cycle_asap.clear();
+				map_instr_cycle_alap.clear();
+				greatest_cycle = 0;
+				cycle = 0;
+				inst_list.clear();
+
+				errs() << "\n\n-- Basic Block: " << ++cont_bb << " --\n";
+
+				filterInstructions(llvm_function, bb_llvm);
+
+				//run on basick block instructions
+				runOnInstructions(llvm_function, bb_llvm);
+			}
+		}
+
+		//run on each file function
+		virtual bool runOnFunction(Function &llvm_function) {
+			//print funcion name
+			errs() << "Function: " << llvm_function.getName() << "\n";
+			//run on each function basic block 
+			runOnBasicBlocks(llvm_function);
+			
+			return false;
 		}
 	};
 } // namespace llvm
