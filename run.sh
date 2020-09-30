adc165af8e5610b191ff5da74517c9aed2a1c6ef
Lavinia Miranda <laviniameds@icloud.com>
2020-09-29 23:35:59 -0300

    rename run script

--- run.sh
+++ run.sh
@@ -12,6 +12,6 @@ clang-10 -S -emit-llvm ${src} -g3 -O0 -Xclang -disable-O0-optnone -o ${obj}
 
 opt-10 -S -mem2reg ${obj} > ${opt}
 
-opt -analyze -dot-cfg ${obj}
+opt -analyze -dot-cfg ${opt}
 
 opt-10 -load build/parallelism/libParallelismPass.so -parallelism < ${opt} > /dev/null
